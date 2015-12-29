5030c852a1d0dae13bf31b25a3fac00fb0dd1973
Salvatore Polito <thedarkhacker94@gmail.com>
2015-12-29 16:53:03 +0100

    gestione file
    prime prove di gestione di file in modalità scrittura e lettura

--- es1_n6.cpp
+++ es1_n6.cpp
@@ -8,17 +8,32 @@ using std::string;
 
 #include<fstream>
 using std::ofstream;
+using std::ios;
 
 #include"Persona.cpp"
 
 int main () {
     Persona p1("salvo", "polito", "licata");
+    Persona p2("tizio", "caio", "---");
 
     ofstream scrivi("prova.txt");
+
     scrivi << p1.getNome() << endl
         <<p1.getCognome() << endl
-        <<p1.getIndirizzo() << endl;
+        <<p1.getIndirizzo()  << endl;
+
 
     scrivi.close();
+
+
+    scrivi.open("prova.txt", ios::in|ios::end);
+
+    scrivi << p2.getNome() << endl
+        <<p2.getCognome() << endl
+        <<p2.getIndirizzo()  << endl;
+
+
+    scrivi.close();
+
     return 0;
 }
